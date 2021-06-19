c5c5d7ffeeaac914e06c6fac132ad0a77d4cfc46
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-06-19 14:54:15 +0430

    Update config.py

--- semantic_segmentation/config.py
+++ semantic_segmentation/config.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # DATASET PARAMETERS
 DATASET = 'nyudv2'
-TRAIN_DIR = '/data/nyud'  # 'Modify data path'
+TRAIN_DIR = '/content/drive/MyDrive/datasets/nyudv2'  # 'Modify data path'
 VAL_DIR = TRAIN_DIR
 TRAIN_LIST = './data/nyudv2/train.txt'
 VAL_LIST = './data/nyudv2/val.txt'
@@ -16,7 +16,7 @@ NORMALISE_PARAMS = [1./255,  # Image SCALE
                     np.array([0.485, 0.456, 0.406]).reshape((1, 1, 3)),  # Image MEAN
                     np.array([0.229, 0.224, 0.225]).reshape((1, 1, 3)),  # Image STD
                     1./5000]  # Depth SCALE
-BATCH_SIZE = 6
+BATCH_SIZE = 4
 NUM_WORKERS = 16
 NUM_CLASSES = 40
 LOW_SCALE = 0.5
